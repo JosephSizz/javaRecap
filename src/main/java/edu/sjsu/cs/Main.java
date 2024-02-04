03f2c014008c1e79d0fa1ab86c571605a6c7ce54
joseph <joseph.hawkins@sjsu.edu>
2024-02-04 14:52:58 -0800

    first attempt

--- src/main/java/edu/sjsu/cs/Main.java
+++ src/main/java/edu/sjsu/cs/Main.java
@@ -4,6 +4,10 @@ package edu.sjsu.cs;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        Candidate candidate = new Candidate(1, "joseph", "hawkins", "coolParty");
+        candidate.printInfo();
+
+        Voter voter = new Voter(12, "luffy", "monkey", true);
+        voter.printInfo();
     }
 }
